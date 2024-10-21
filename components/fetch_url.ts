bf9ae950b9e009d0ddcbfc024d2194772f1f0a32
pbaekgaard <peter.baekgaard@outlook.com>
2024-10-21 15:35:09 +0200

    init

--- components/fetch_url.ts
+++
@@ -1,5 +0,0 @@
-export async function fetch_url(url: string, userAgentName: string): Promise<string> {
-    const res = await fetch(url, {headers: {'User-Agent': userAgentName}})
-    const page_html = await res.text()
-    return page_html
-}
